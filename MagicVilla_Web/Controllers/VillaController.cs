a5d37d8f55ab3ea25890e85592e38f4a245619c7
Sambidhan Shah <sambidhan26@gmail.com>
2025-06-28 17:41:12 +0545

    The final Cmmit before reboot

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -17,17 +17,39 @@ namespace MagicVilla_Web.Controllers
             _services = services;
             _mapper = mapper;
         }
+
         public async Task<IActionResult> IndexVilla()
         {
             List<VillaDTO> list = new();
             var response = await _services.GetAllAsync<APIResponse>();
 
-            if(response != null && response.IsSuccess) 
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
 
             }
             return View(list);
         }
+        public async Task<IActionResult> CreateVilla()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if(ModelState.IsValid) 
+            {
+                var response = await _services.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess) 
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
+
     }
 }
